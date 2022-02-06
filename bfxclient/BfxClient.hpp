5a4d566f6c47940cf8943ec55b74f8edcf918495
SpacRocket <spacrocket@bat.europe-west2-a.c.southern-bonsai-333121.internal>
2022-02-06 13:09:31 +0000

    Change library from PUBLIC to INTERFACE

--- bfxclient/BfxClient.hpp
+++ bfxclient/BfxClient.hpp
@@ -18,6 +18,7 @@
 #include <cstdlib>
 #include <optional>
 
+
 namespace FIX{
 
 #ifdef HAVE_SSL
@@ -49,3 +50,68 @@ public:
         std::optional<FIX::ScreenLogFactory> logFactory;
 }; //End of BfxClient
 }
+
+// Constructors
+FIX::BfxClient::BfxClient() {
+  const char* bfxconf{std::getenv("BFX_CLIENT_CONF")};
+  if (bfxconf == nullptr) {
+    throw FIX::ConfigError(
+        "$BFX_CLIENT_CONF is not configured. Use different constructor or set "
+        "env variable.");
+  }
+
+  settings = FIX::SessionSettings(bfxconf);
+  application.settings = settings;
+  storeFactory = FIX::FileStoreFactory(settings);
+  logFactory = FIX::ScreenLogFactory(settings);
+
+  initiator = new FIX::SocketInitiator(application, storeFactory.value(),
+                                       settings, logFactory.value());
+}
+
+FIX::BfxClient::BfxClient(bool logging) {
+  const char* bfxconf{std::getenv("BFX_CLIENT_CONF")};
+  if (bfxconf == nullptr) {
+    throw FIX::ConfigError(
+        "$BFX_CLIENT_CONF is not configured. Use different constructor or set "
+        "env variable.");
+  }
+
+  settings = FIX::SessionSettings(bfxconf);
+  application.settings = settings;
+
+  if (logging) {
+    storeFactory = FIX::FileStoreFactory(settings);
+    logFactory = FIX::ScreenLogFactory(settings);
+
+    initiator = new FIX::SocketInitiator(application, storeFactory.value(),
+                                         settings, logFactory.value());
+  } else {
+    initiator = new FIX::SocketInitiator(application, storeFactory.value(),
+                                         settings, logFactory.value());
+  }
+}
+
+FIX::BfxClient::~BfxClient() {}
+
+#ifdef HAVE_SSL
+FIX::BfxClient::BfxClient::BfxClient(std::string configFilePath,
+                                     FIX::SSLMode mode) {
+  settings = FIX::SessionSettings(configFilePath);
+
+  application.settings = settings;
+
+  storeFactory = FIX::FileStoreFactory(settings);
+  logFactory = FIX::ScreenLogFactory(settings);
+
+  if (mode == FIX::SSLMode::SSL)
+    initiator = new FIX::ThreadedSSLSocketInitiator(
+        application, storeFactory.value(), settings, logFactory.value());
+  else if (mode == FIX::SSLMode::SSL_ST)
+    initiator = new FIX::SSLSocketInitiator(application, storeFactory.value(),
+                                            settings, logFactory.value());
+  else if (mode == FIX::SSLMode::None)
+    initiator = new FIX::SocketInitiator(application, storeFactory.value(),
+                                         settings, logFactory.value());
+}
+#endif
